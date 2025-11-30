7a7b8ad9d90984754dbb11bc2b18dc8d8ca96832
Manikanta6205 <pendelamanikanta6205@gmail.com>
2025-11-30 17:06:48 +0530

    Made all the codes ready

--- CN/checksum.cpp
+++ CN/checksum.cpp
@@ -1,87 +1,43 @@
 #include <iostream>
 using namespace std;
-
-// Function to add two binary strings (1's complement addition)
-string addBinary(string a, string b) {
-    string result = "";
-    int carry = 0;
-
-    for (int i = a.size() - 1; i >= 0; i--) {
-        int bit1 = a[i] - '0';
-        int bit2 = b[i] - '0';
-
-        int sum = bit1 + bit2 + carry;
-
-        result = char((sum % 2) + '0') + result;
-        carry = sum / 2;
+string addB(string a,string b){
+    string res="";int cr=0;
+    for(int i=a.size()-1;i>=0;i--){
+        int b1=a[i]-'0';
+        int b2=b[i]-'0';
+        int s=b1+b2+cr;
+        res=char((s%2)+'0')+res;
+        cr=s/2;
     }
-
-    // If there is a carry, wrap around (add it to LSB)
-    if (carry == 1) {
-        // add 00000001
-        string one = string(a.size() - 1, '0') + "1";
-        return addBinary(result, one);
+    if(cr==1){
+        string one=string(a.size()-1,'0')+"1";
+        return addB(res,one);
     }
-
-    return result;
+    return res;
 }
-
-// Function to take 1's complement (flip bits)
-string onesComplement(string s) {
-    for (int i = 0; i < s.size(); i++) {
-        s[i] = (s[i] == '0') ? '1' : '0';
-    }
+string oneC(string s){
+    for(int i=0;i<s.size();i++)s[i]=(s[i]=='0')?'1':'0';
     return s;
 }
-
-int main() {
-    int n;
-    cout << "Enter number of 8-bit blocks: ";
-    cin >> n;
-
+int main(){
+    int n;cout<<"Enter number of 8-bit blocks: ";cin>>n;
     string data[100];
-
-    cout << "Enter each 8-bit binary block:\n";
-    for (int i = 0; i < n; i++) {
-        cin >> data[i];
-    }
-
-    // -------------- SENDER SIDE -----------------
-    string sum = data[0];
-
-    // Add all blocks
-    for (int i = 1; i < n; i++) {
-        sum = addBinary(sum, data[i]);
-    }
-
-    // Checksum = 1's complement of sum
-    string checksum = onesComplement(sum);
-
-    cout << "\n--- SENDER SIDE ---\n";
-    cout << "Final Sum:   " << sum << endl;
-    cout << "Checksum:    " << checksum << endl;
-
-    // -------------- RECEIVER SIDE -----------------
-    cout << "\nEnter received blocks (including checksum):\n";
+    cout<<"Enter each 8-bit binary block:\n";
+    for(int i=0;i<n;i++)cin>>data[i];
+    string sum=data[0];
+    for(int i=1;i<n;i++)sum=addB(sum,data[i]);
+    string chks=oneC(sum);
+    cout<<"\n--- SENDER SIDE ---\n";
+    cout<<"Final Sum:   "<<sum<<endl;
+    cout<<"Checksum:    "<<chks<<endl;
+    cout<<"\nEnter received blocks (including checksum):\n";
     string recv[100];
-    for (int i = 0; i < n + 1; i++) {
-        cin >> recv[i];
-    }
-
-    string rsum = recv[0];
-
-    for (int i = 1; i < n + 1; i++) {
-        rsum = addBinary(rsum, recv[i]);
-    }
-
-    // Final complement
-    string finalCheck = onesComplement(rsum);
-
-    cout << "\n--- RECEIVER SIDE ---\n";
-    if (finalCheck == string(finalCheck.size(), '0'))
-        cout << "Status: No Error (Data is correct)\n";
-    else
-        cout << "Status: ERROR detected (Data corrupted)\n";
-
+    for(int i=0;i<n+1;i++)cin>>recv[i];
+    string rsum=recv[0];
+    for(int i=1;i<n+1;i++)rsum=addB(rsum,recv[i]);
+    string fchk=oneC(rsum);
+    cout<<"\n--- RECEIVER SIDE ---\n";
+    if(fchk==string(fchk.size(),'0'))cout<<"Status: No Error (Data is correct)\n";
+    else cout<<"Status: ERROR detected (Data corrupted)\n";
     return 0;
 }
--- CN/parity.cpp
+++ CN/parity.cpp
@@ -1,75 +1,46 @@
 #include <iostream>
 using namespace std;
 
-// Function to count number of 1s
-int countOnes(string s) {
-    int count = 0;
-    for(char c : s) {
-        if(c == '1') count++;
+int cnt1(string s){
+    int c=0;
+    for(char x:s){
+        if(x=='1')c++;
     }
-    return count;
+    return c;
 }
 
-int main() {
+int main(){
     string data;
-    int choice;
-
-    // -------------- SENDER SIDE -----------------
-    cout << "Enter binary string: ";
-    cin >> data;
-
-    cout << "Choose Parity Type:\n";
-    cout << "1. Even Parity\n";
-    cout << "2. Odd Parity\n";
-    cin >> choice;
-
-    int ones = countOnes(data);
-    char parityBit;
-
-    // EVEN PARITY: total 1s must be even
-    if (choice == 1) {
-        if (ones % 2 == 0)
-            parityBit = '0';    // already even
-        else
-            parityBit = '1';    // make it even
+    int ch;
+    cout<<"Enter binary string: ";
+    cin>>data;
+    cout<<"Choose Parity Type:\n";
+    cout<<"1. Even Parity\n";
+    cout<<"2. Odd Parity\n";
+    cin>>ch;
+    int ones=cnt1(data);
+    char pbit;
+    if(ch==1){
+        if(ones%2==0)pbit='0';
+        else pbit='1';
+    }else if(ch==2){
+        if(ones%2==0)pbit='1';
+        else pbit='0';
     }
-
-    // ODD PARITY: total 1s must be odd
-    else if (choice == 2) {
-        if (ones % 2 == 0)
-            parityBit = '1';    // make it odd
-        else
-            parityBit = '0';    // already odd
-    }
-
-    // Append the parity bit
-    string transmitted = data + parityBit;
-
-    cout << "\n--- SENDER SIDE ---\n";
-    cout << "Original Data:   " << data << endl;
-    cout << "Parity Bit:      " << parityBit << endl;
-    cout << "Transmitted Data: " << transmitted << endl;
-
-    // ---------------- RECEIVER SIDE ----------------
-    string received;
-    cout << "\nEnter received binary string: ";
-    cin >> received;
-
-    int receivedOnes = countOnes(received);
-
-    bool isCorrect;
-
-    if (choice == 1) {  // Even parity check
-        isCorrect = (receivedOnes % 2 == 0);
-    } else {            // Odd parity check
-        isCorrect = (receivedOnes % 2 == 1);
-    }
-
-    cout << "\n--- RECEIVER SIDE ---\n";
-    if (isCorrect)
-        cout << "Status: No Error (Data is correct)\n";
-    else
-        cout << "Status: ERROR detected (Data corrupted)\n";
-
+    string trns=data+pbit;
+    cout<<"\n--- SENDER SIDE ---\n";
+    cout<<"Original Data:   "<<data<<endl;
+    cout<<"Parity Bit:      "<<pbit<<endl;
+    cout<<"Transmitted Data: "<<trns<<endl;
+    string recv;
+    cout<<"\nEnter received binary string: ";
+    cin>>recv;
+    int rc1=cnt1(recv);
+    bool corr;
+    if(ch==1)corr=(rc1%2==0);
+    else corr=(rc1%2==1);
+    cout<<"\n--- RECEIVER SIDE ---\n";
+    if(corr)cout<<"Status: No Error (Data is correct)\n";
+    else cout<<"Status: ERROR detected (Data corrupted)\n";
     return 0;
 }
